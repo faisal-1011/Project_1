f9ca469b9bd7c24e11e8414568df43bd5bb0893f
Your Name <you@example.com>
2025-08-09 04:32:06 -0700

    Adding New Project

---
+++ test.py
@@ -0,0 +1,12 @@
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
+
+
+result = add_numbers(3, 5)
+print("Sum:", result)
+
+difference = subtract_numbers(3, 5)
+print("Difference:", difference)
